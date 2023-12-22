f9e90d49a37bf915bdaef12a6195756b9b6e0f31
Benjamin Shvartsman <86971030+frogsterr@users.noreply.github.com>
2023-12-22 00:00:33 -0500

    Update popup.js
    Deleted redundant functions.

--- frontend/popup.js
+++ frontend/popup.js
@@ -16,17 +16,17 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    
+    //Background.js -> turns ON service
     videoButton.addEventListener('click', () => {
         chrome.runtime.sendMessage({command: "videoOnButtonClicked"});
     });
 
-    
+    //Background.js -> turns OFF service
     videoOffButton.addEventListener('click', () => {
         chrome.runtime.sendMessage({command: "videoOffButtonClicked"});
     }); 
 
-
+    //Opens Options tab
     optionsButton.addEventListener('click', () => {
         //Create New Chrome Tab
         chrome.tabs.create({url:'options.html', pinned:false, active: true});
@@ -42,70 +42,3 @@ const onOffSwitch = (onButton, offButton) => {
         offButton.classList.add("btn-outline-dark");
         };
 }
-
-//Establishes websocket connection
-const establishConnection = endpoint => {
-    //Creates socket object
-    videoSocket = new WebSocket(endpoint);
-
-    //Connects to endpoint and verifies with text overlay
-    videoSocket.addEventListener('open', () => {
-        popupText.textContent = 'Connected to server!';
-        document.body.appendChild(popupText);
-        sendData(videoSocket, "Client has connected!");
-        websocketExists = true;
-
-    });
-
-    //
-    videoSocket.addEventListener("message", (r) => {
-        console.log(r);
-        popupText.textContent = `${r.data}`;
-    });
-};
-
-//Turns off websocket connection
-const turnOffConnection = socket => {
-    if (websocketExists) {
-        socket.close();
-        websocketExists = false;
-        popupText.textContent = 'Disconnected from server.';
-        console.log("connection closed");
-    } else {
-        console.log("Connection Doesn't exist");
-    }
-};
-
-//Sends message to socket
-function sendData(socket, message) {
-    if (socket.readyState === WebSocket.OPEN) {
-        socket.send(message);
-    } else {
-        popupText.textContent = 'Error sending message!';
-    };
-};
-
-//Stops video stream
-function stopVideoStream() {
-        var tracks = videoStream.getTracks();
-        tracks.forEach((track) => {
-            track.stop();
-        });
-        videoStream = null;
-
-        //REMOVE LATER
-        popupText.textContent = 'Closed Stream';
-};
-
-//Starts video stream
-const startVideoStream = () => {
-    navigator.mediaDevices.getUserMedia({video: true}).then(function(stream) {
-
-        //REMOVE LATER
-        videoElement.srcObject = stream;
-
-        videoStream = stream;
-    }).catch(function() {
-        alert('Unable to capture your camera.');
-    });
-};
